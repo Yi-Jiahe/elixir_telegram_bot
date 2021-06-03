c9eca5875a0a1d46daf478d3e066b3d99443ad81
yijiahe <>
2021-06-03 14:06:52 +0000

    Added markdown and improved inline help text

--- lib/homunculus/commands.ex
+++ lib/homunculus/commands.ex
@@ -1,4 +1,11 @@
 defmodule Homunculus.Commands do
+  @moduledoc """
+  Handles commands sent to the bot
+  """
+  
+  @doc """
+  Provides information on how to use the bot
+  """
   def start(message) do
     %{"chat" => %{"id" => chat_id}, "text" => text} = message
     case String.split(text) do
@@ -6,14 +13,15 @@ defmodule Homunculus.Commands do
         params = Enum.join(tail, " ")
         case params do
           "inline_help" ->
-            request_params = %{"chat_id" => chat_id, "text" => "Inline commands:\n rot13 [message]"}
-            {:ok, request_body} = JSON.encode(request_params)
+            {:ok, request_body} = JSON.encode(%{"chat_id" => chat_id, "text" => "Inline commands:\n`rot13 [message]`\n\tApplies a ROT13 subsitution cipher to `message`", "parse_mode" => "markdown"})
             {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/sendMessage", [], 'application/json', request_body}, [], [])
+            # switch_inline_query button immediately switches user back to their original chat without giving time for them to read the documentation
+            #{:ok, request_body} = JSON.encode(%{"chat_id" => chat_id, "text" => "Inline commands:\n rot13 [message]\n\tApplies a ROT13 subsitution cipher to the message", "reply_markup" => %{"inline_keyboard" => [[%{"text" => "Use me!", "switch_inline_query" => ""}]]}})
+            #{:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/sendMessage", [], 'application/json', request_body}, [], [])
           _ -> nil
         end
       _ -> nil
     end
-    IO.inspect(message)
   end
 
   def greet(message) do
--- lib/homunculus/inline.ex
+++ lib/homunculus/inline.ex
@@ -1,8 +1,8 @@
 defmodule Homunculus.Inline do
   def help(inline_query) do
     %{"id" => id} = inline_query
-    {:ok, request_params} = JSON.encode(%{"inline_query_id" => id, "results" => [], "switch_pm_text" => "Get help with inline commands", "switch_pm_parameter" => "inline_help"})
-    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/answerInlineQuery", [], 'application/json', request_params}, [], [])
+    {:ok, request_body} = JSON.encode(%{"inline_query_id" => id, "results" => [], "switch_pm_text" => "Get help with inline commands", "switch_pm_parameter" => "inline_help"})
+    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/answerInlineQuery", [], 'application/json', request_body}, [], [])
   end
 
   def rot13(inline_query) do
@@ -12,9 +12,9 @@ defmodule Homunculus.Inline do
         {:ok, text} = Rot13.encode(Enum.join(tail, " "))
         results = [%{"type" => "article", "id" => 1, "title" => "Encode", "input_message_content" => %{"message_text" => text}, "description" => text}]
         IO.inspect(results)
-        {:ok, request_params} = JSON.encode(%{"inline_query_id" => id, "results" => results})
-        IO.inspect(request_params)
-        {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/answerInlineQuery", [], 'application/json', request_params}, [], [])
+        {:ok, request_body} = JSON.encode(%{"inline_query_id" => id, "results" => results})
+        # IO.inspect(request_body)
+        {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, _body}} = :httpc.request(:post, {Application.fetch_env!(:born_from_a_bottle_bot, :base_url) <> "/answerInlineQuery", [], 'application/json', request_body}, [], [])
       _ -> nil
     end
   end
